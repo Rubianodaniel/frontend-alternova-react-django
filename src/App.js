83978ed7e5dc3d2dee7659b5607490079bfde5a1
Rubianodaniel <danielrubianomeneses@gmail.com>
2022-12-26 14:01:25 -0500

    primer commit haciendo el frontend de la epi peliculas por medio de react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import MovieList from "./components/Movies/movies_list"; 
+
+function App() {
+  return (
+    <>
+    <MovieList/>
+    </>
+  );
+}
+
+export default App;
